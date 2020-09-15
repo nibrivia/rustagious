10b67c352d73e8abc99a005a95d5edc21f5e3294
Olivia Brode-Roger <nibr@mit.edu>
2020-09-15 15:16:24 -0400

    lots

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,12 @@
 //! A crate to model contagion
 
 use rand::distributions::{Bernoulli, Distribution};
+use rand_distr::LogNormal;
 
 type Time = u64;
+
+const SYMPTOMATIC_MEAN: f64 = 5.058146;
+const SYMPTOMATIC_SD: f64 = 1.518921;
 
 /// Struct representing an individual and keeping track of associated state
 #[derive(Debug)]
@@ -24,6 +28,7 @@ pub struct Infection {
 
     testable_date: Time,
     contagious_date: Time,
+    recovery_date: Time,
 
     symptomatic_date: Option<Time>,
 
@@ -44,18 +49,32 @@ impl Person {
 
     /// Exposes a person to a source on a given date
     pub fn expose(self: &mut Self, date: Time, source: String) {
+        // already infected, let's not do this again...
         if self.infection.is_some() {
             return;
         }
+
+        let mut rng = rand::thread_rng();
+
+        // Symptomatic date, everything is computed in reference to that
+        // mean 2, standard deviation 3
+        let log_normal = LogNormal::new(SYMPTOMATIC_MEAN.ln(), SYMPTOMATIC_SD.ln()).unwrap();
+        let symptomatic_date: Time = date + log_normal.sample(&mut rng).round() as Time;
+        let testable_date = symptomatic_date - 2;
+        let contagious_date = symptomatic_date - 2;
+        let recovery_date = symptomatic_date + 14;
+
+        // Do we show symptoms
         let d = Bernoulli::new(0.6).unwrap();
-        let v = d.sample(&mut rand::thread_rng());
-        let symptomatic_date = if v { Some(date + 5) } else { None };
+        let v = d.sample(&mut rng);
+        let symptomatic_date = if v { Some(symptomatic_date) } else { None };
 
         self.infection = Some(Infection {
             date,
-            testable_date: date + 2,   // TODO
-            contagious_date: date + 3, // TODO
+            testable_date,
+            contagious_date,
             symptomatic_date,
+            recovery_date,
             source,
         });
     }
@@ -84,7 +103,7 @@ impl Person {
     /// Is this person able to infect others?
     pub fn is_contagious(self: &Self, date: Time) -> bool {
         if let Some(infection) = &self.infection {
-            return infection.contagious_date <= date;
+            return infection.contagious_date <= date && date <= infection.recovery_date;
         }
         false
     }
@@ -110,6 +129,7 @@ impl Person {
 
     /// Has this person *ever* been infected?
     pub fn was_sick(self: &Self, date: Time) -> bool {
+        println!("{} {:?}", self.name, self.infection);
         if let Some(infection) = &self.infection {
             return infection.date <= date;
         }
@@ -124,7 +144,7 @@ impl Person {
                 self.name,
                 infection.date,
                 infection.source,
-                self.is_isolating(99999)
+                self.is_isolating(99999) // TODO
             );
         } else {
             println!("{} healthy", self.name);
@@ -132,10 +152,29 @@ impl Person {
     }
 }
 
-/// Returns the odds of a person being contagious `t` after infection
-pub fn p_contagious(_t: Time) -> f64 {
-    // TODO
-    1.0
+/// Phase type
+#[derive(Debug, PartialEq, Eq)]
+pub enum Phase {
+    /// B sees A
+    A,
+
+    /// B is isolating
+    Isolate,
+
+    /// B sees C
+    C,
+}
+
+/// An example phase function
+pub fn phase(day: u64) -> Phase {
+    let cycle_day = day % (6 * 7);
+    if cycle_day <= 15 {
+        Phase::A
+    } else if cycle_day >= 21 && cycle_day <= 36 {
+        Phase::C
+    } else {
+        Phase::Isolate
+    }
 }
 
 #[cfg(test)]
@@ -148,18 +187,44 @@ mod test {
         assert_eq!(me.name, "Olivia".to_string());
 
         // Get sick
-        me.expose(3, "MIT".to_string());
+        me.expose(2, "MIT".to_string());
 
         // Don't know better yet, should *not* be isolating...
-        assert!(!me.is_isolating(4));
+        assert!(!me.is_isolating(2));
         assert!(me.was_sick(4)); // ...but am sick (hidden state)
 
         // Get tested
-        me.test(5);
+        me.test(12);
 
         // Really should be isolating
-        assert!(me.is_isolating(6));
-        assert!(me.was_sick(6));
+        assert!(me.is_isolating(12));
+        assert!(me.was_sick(12));
+    }
+
+    #[test]
+    fn interaction_abc_future() {
+        let mut a = Person::new("Olivia A".to_string());
+        let mut b = Person::new("Olivia B".to_string());
+        let mut c = Person::new("Olivia C".to_string());
+        assert!(!a.was_sick(0));
+        assert!(!b.was_sick(0));
+        assert!(!c.was_sick(0));
+
+        // Sick me gets sick
+        b.expose(10, "MIT".to_string());
+        assert!(b.was_sick(10));
+
+        // Nobody's sick, but out of order
+        b.interact(1, &mut a);
+        assert!(!a.was_sick(1));
+        assert!(!b.was_sick(1));
+        assert!(!c.was_sick(1));
+
+        // Interact
+        b.interact(19, &mut c);
+        assert!(!a.was_sick(19)); // should not be sick
+        assert!(b.was_sick(19)); // should *also* be sick
+        assert!(c.was_sick(19)); // should *also* be sick
     }
 
     #[test]
@@ -174,8 +239,33 @@ mod test {
         assert!(sick_me.was_sick(2));
 
         // Interact
-        sick_me.interact(3, &mut healthy_me);
-        assert!(sick_me.was_sick(4)); // should still be sick
-        assert!(healthy_me.was_sick(4)); // should *also* be sick
+        sick_me.interact(10, &mut healthy_me);
+        assert!(sick_me.was_sick(11)); // should still be sick
+        assert!(healthy_me.was_sick(11)); // should *also* be sick
+    }
+
+    #[test]
+    fn phase_test() {
+        assert_eq!(phase(0), Phase::A, "0 with A");
+        assert_eq!(phase(1), Phase::A, "1 with A");
+        assert_eq!(phase(15), Phase::A, "15 with A");
+
+        assert_eq!(phase(16), Phase::Isolate, "16 isolating");
+        assert_eq!(phase(17), Phase::Isolate, "17 isolating");
+        assert_eq!(phase(18), Phase::Isolate, "18 isolating");
+        assert_eq!(phase(19), Phase::Isolate, "19 isolating");
+        assert_eq!(phase(20), Phase::Isolate, "20 isolating");
+
+        assert_eq!(phase(21), Phase::C, "21 C");
+        assert_eq!(phase(30), Phase::C, "30 C");
+        assert_eq!(phase(36), Phase::C, "36 C");
+
+        assert_eq!(phase(37), Phase::Isolate, "37 isolating");
+        assert_eq!(phase(38), Phase::Isolate, "38 isolating");
+        assert_eq!(phase(39), Phase::Isolate, "39 isolating");
+        assert_eq!(phase(40), Phase::Isolate, "40 isolating");
+        assert_eq!(phase(41), Phase::Isolate, "41 isolating");
+
+        assert_eq!(phase(42), Phase::A, "42 with A");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,42 @@
 
 //! ???
 
-use rustagious::Person;
+use rustagious::{phase, Person, Phase};
 use std::collections::HashMap;
 
-type Res = (bool, bool, bool);
+type Res = (String, String, String);
 
 fn main() {
-    let res = run_n(100);
-    let mut tot = 0;
-    for (k, v) in &res {
-        println!("{:?}: {:?}", v, k);
-        tot += v;
+    let res = run_n(100_000);
+    let mut ord = Vec::new();
+
+    //let mut tot = 0;
+    for (k, v) in res {
+        ord.push((v, k));
+        //tot += v;
     }
-    println!("{} runs total", tot);
-    println!("done");
+    ord.sort();
+
+    for (v, k) in &ord {
+        println!(
+            "{:.0} {} {} {}",
+            //(500. * (*v as f64)) / tot as f64,
+            v,
+            k.0,
+            k.1,
+            k.2,
+            //k.3,
+            //k.4
+        );
+    }
+    //printerr!("done");
 }
 
 fn run_n(n: u64) -> HashMap<Res, u64> {
     let mut res = HashMap::new();
 
     for day in 0..6 * 7 {
-        for source in 0..3 {
+        for source in 1..4 {
             for _ in 0..n {
                 let run = run_trial(day, source);
                 let cur = res.get(&run).or_else(|| Some(&0)).unwrap() + 1;
@@ -34,52 +49,49 @@ fn run_n(n: u64) -> HashMap<Res, u64> {
     res
 }
 
-enum Phase {
-    A,
-    Isolate,
-    C,
-}
-
-fn phase(day: u64) -> Phase {
-    let cycle_day = day % 6 * 7;
-    if cycle_day <= 15 {
-        Phase::A
-    } else if cycle_day >= 21 && cycle_day <= 36 {
-        Phase::C
-    } else {
-        Phase::Isolate
-    }
-}
-
 /// Runs a single experiment
 fn run_trial(moment: u64, who: u64) -> Res {
+    //let mut z = Person::new("0".to_string());
     let mut a = Person::new("A".to_string());
     let mut b = Person::new("B".to_string());
     let mut c = Person::new("C".to_string());
+    //let mut d = Person::new("D".to_string());
 
-    let source = "alpha".to_string();
     match who {
-        0 => a.expose(moment, source),
-        1 => {
-            b.expose(moment, source);
-        }
-        2 => c.expose(moment, source),
+        //0 => z.expose(moment, format!("Z.{:}", moment)),
+        1 => a.expose(moment, format!("A.{:}", moment)),
+        2 => b.expose(moment, format!("B.{:}", moment)),
+        3 => c.expose(moment, format!("C.{:}", moment)),
+        //4 => d.expose(moment, format!("D.{:}", moment)),
         _ => unreachable!(),
     }
 
-    for day in 1..300 {
-        if a.is_isolating(day) || b.is_isolating(day) || c.is_isolating(day) {
+    for day in 0..300 {
+        if
+        //z.is_isolating(day) ||
+        a.is_isolating(day) || b.is_isolating(day) || c.is_isolating(day)
+        //|| d.is_isolating(day)
+        {
             break;
         }
 
         match day % 7 {
-            1 | 4 => a.test(day),
+            2 | 5 => {
+                a.test(day);
+            }
+            3 | 6 => {
+                //z.test(day);
+            }
             _ => {}
         }
 
+        //c.interact(day, &mut d);
         match phase(day) {
             Phase::A => b.interact(day, &mut a),
-            Phase::C => b.interact(day, &mut c),
+            Phase::C => {
+                // a.interact(day, &mut z);
+                b.interact(day, &mut c);
+            }
             Phase::Isolate => {}
         }
     }
@@ -92,5 +104,19 @@ fn run_trial(moment: u64, who: u64) -> Res {
     //println!("B: {:?}", b.get_infection());
     //println!("C: {:?}", c.get_infection());
 
-    (a.was_sick(100), b.was_sick(100), c.was_sick(100))
+    (
+        //get_source(&z),
+        get_source(&a),
+        get_source(&b),
+        get_source(&c),
+        //get_source(&d),
+    )
+}
+
+fn get_source(p: &Person) -> String {
+    if let Some(infection) = p.get_infection() {
+        infection.source.to_string()
+    } else {
+        "-.00".to_string()
+    }
 }
